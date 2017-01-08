ee3f05a92c5b409b1e1ff2c1e7a50954ab2d72cb
Jon Bardin <diclophis@gmail.com>
2017-01-07 20:11:58 -0800

    this might work on an ec2 server

--- README.sh
+++ README.sh
@@ -11,8 +11,10 @@ sudo pkill -9 -f deserialize; sudo kill -9 $(pidof -o 1 init) $(pidof docker) $(
 #sudo modprobe xt_conntrack
 #sudo modprobe nf_conntrack
 #sudo modprobe ip_tables
+
 #sudo lxc-destroy --name deb001
 #sudo rm -Rf /var/tmp/deb001
-#sudo ./haconiwa create garden-of-kubeadm.haco
+
+sudo ./haconiwa create garden-of-kubeadm.haco
 
 sudo ./haconiwa start garden-of-kubeadm.haco /sbin/init
